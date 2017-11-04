0e57f6f7a6892feb851fca549f2b27e6d61c21cc
Carrie Valentin <valentin.carrie@etu.u-bordeaux.fr>
2017-11-04 19:14:56 +0100

    Added research function and updated package.json

--- app/Rate.php
+++ app/Rate.php
@@ -13,4 +13,20 @@ class Rate extends Model
     public static function getByIdAccount($idAccount){
         return static::where('idAccount', '=', $idAccount);
     }
+
+    public static function averageForList($id){
+        $all_rates = Rate::getByIdList($id)->get();
+        $cumul = 0;
+
+        foreach ($all_rates as $rate){
+            $cumul += $rate->rating;
+        }
+        if (count($all_rates)!=0){
+            $cumul /= count($all_rates);
+        }else{
+            $cumul = null;
+        }
+
+        return $cumul;
+    }
 }
