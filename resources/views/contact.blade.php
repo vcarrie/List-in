8c6e6bbb701ffb3be69c159f54e2ce43d5a118ad
Cedric Frecheville <frecheville.cedric@gmail.com>
2017-11-06 10:51:09 +0100

    add contact.js and contact.blade.php updated

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,12 @@
-@extends(template.master)
+@extends(template.main)
 
 @section('title', 'Nous contacter')
 
 @section('mid-content')
+
+<div class="text">
+  <p> Des idées, des remarques, des questions ? Faites en nous part !</p>
+</div>
 
 <div class="contactForm">
   <form method="post">
@@ -13,4 +17,10 @@
     <p><input type="submit" value="Envoyer" /></p>
   </form>
 </div>
+
+<div class="autreContact">
+  <p> Vous pouvez également nous contacter par téléphone au : </p>
+  <p> ou bien directement par mail à l'adresse  : listin@gmail.com</p>
+</div>
+
 @endsection
