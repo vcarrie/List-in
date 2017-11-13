f783862a854d3b3a5175502edf043b76f44fcafe
Cedric Frecheville <frecheville.cedric@gmail.com>
2017-11-13 18:11:38 +0100

    view contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,9 +6,11 @@
 
 @section('main')
     <br>
-    <div class="container">
+    <div class="container" style="margin: 2rem; text-align: center; padding-left:4rem; padding-right:4rem; max-width:500px; margin:0px auto;">
         <div class="row card text-white bg-dark">
-            <h4 class="card-header">Contactez-moi</h4>
+            <h1 class="card-header" style="font-weight:bold">Contactez-nous</h1>
+            <h4 class="card-header">Des idées, des remarques, des questions ?</h4>
+            <h4 class="card-header">Faites-nous en part !</h4>
             <div class="card-body">
                 <form action="{{ url('contact') }}" method="POST">
                     {{ csrf_field() }}
@@ -21,10 +23,10 @@
                         {!! $errors->first('email', '<div class="invalid-feedback">:message</div>') !!}
                     </div>
                     <div class="form-group">
-                        <textarea class="form-control {{ $errors->has('message') ? 'is-invalid' : '' }}" name="message" id="message" placeholder="Votre message">{{ old('message') }}</textarea>
+                        <textarea class="form-control {{ $errors->has('message') ? 'is-invalid' : '' }}" name="message" id="message" placeholder="Votre message" style="height:275px;">{{ old('message') }}</textarea>
                         {!! $errors->first('message', '<div class="invalid-feedback">:message</div>') !!}
                     </div>
-                    <button type="submit" class="btn btn-secondary">Envoyer !</button>
+                    <button type="submit" class="btn btn-secondary" style="background-color: #e03913; color: #fff">Envoyer</button>
                 </form>
             </div>
         </div>
