5b0cf52cada669c1c6744b64b943d8145008e8fa
Cormelier Matthieu <matthieu.cormelier@etu.u-bordeaux.fr>
2017-11-16 14:17:16 +0100

    Revert "Merge branch 'front-end' into 'matthieu'"
    This reverts commit b35a8aed5eae293c7657215bf4c6f20749c0500c

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Mail envoyé')
 
 @section('main')
-    <br>
-    <div class="container">
-        <div class="row card text-white bg-dark">
-            <h1 class="card-header">Contactez-nous</h1>
-            <div class="card-body">
-                <p class="card-text">Merci. Votre message a été transmis à l'administrateur du site. Vous recevrez une réponse rapidement.</p>
-            </div>
-        </div>
+<br>
+<div class="container">
+  <div class="row card text-white bg-dark">
+    <h4 class="card-header">Contactez-moi</h4>
+    <div class="card-body">
+      <p class="card-text">Merci. Votre message a été transmis à l'administrateur du site. Vous recevrez une réponse rapidement.</p>
     </div>
+  </div>
+</div>
 @endsection
