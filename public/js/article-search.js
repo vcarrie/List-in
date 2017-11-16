70b406d84870134707d1e4d1042158372acb83c5
Cormelier Matthieu <matthieu.cormelier@etu.u-bordeaux.fr>
2017-11-16 13:45:24 +0100

    Step 2 forms + submit okay (needs css)

--- public/js/article-search.js
+++ public/js/article-search.js
@@ -65,64 +65,82 @@
             $('.no-article').css("display", "none");
           }
 
-          
-
-          var id_cdiscount = document.createElement('input');
-          id_cdiscount.setAttribute("class", "id_cdiscount");
-          id_cdiscount.setAttribute("type", "hidden");
-          id_cdiscount.setAttribute("name", "product[][id]");
-          id_cdiscount.value = selected_article.Id;
-          console.log(selected_article.Id);
-
-          var quantity = document.createElement('select');
-          quantity.setAttribute("name", "product[][quantity]");
-          for (var i = 1; i < 21; i++) {
-            var option= document.createElement('option');
-            option.setAttribute("id", i);
-            option.text = i;
-            quantity.appendChild(option);
+          var already_exists = false;
+          var double_article = document.getElementsByClassName('id_cdiscount');
+          for (var i = 0; i < double_article.length; i++) {
+            if(double_article[i].value == selected_article.Id){
+              already_exists = true;
+            }
           }
 
-          var selected_article_container = document.createElement('div');
-          selected_article_container.setAttribute("class", "article_container");
+          if(!already_exists){
+            var id_cdiscount = document.createElement('input');
+            id_cdiscount.setAttribute("class", "id_cdiscount");
+            id_cdiscount.setAttribute("type", "hidden");
+            id_cdiscount.setAttribute("name", "product[" + selected_article.Id + "][id]");
+            id_cdiscount.value = selected_article.Id;
 
-          var selected_image = document.createElement('img');
-          selected_image.setAttribute("src", selected_article.MainImageUrl);
+            var quantity = document.createElement('select');
+            quantity.setAttribute("name", "product[" + selected_article.Id + "][quantity]");
+            for (var i = 1; i < 21; i++) {
+              var option= document.createElement('option');
+              option.setAttribute("id", i);
+              option.text = i;
+              quantity.appendChild(option);
+            }
 
-          var selected_name = document.createElement('strong');
-          var selected_name_value = document.createTextNode(selected_article.Name);
-          selected_name.appendChild(selected_name_value);
+            var selected_article_container = document.createElement('div');
+            selected_article_container.setAttribute("class", "article_container");
 
-          var selected_desc = document.createElement('p');
-          var selected_desc_value = document.createTextNode(selected_article.Description);
-          selected_desc.appendChild(selected_desc_value);
+            var selected_image = document.createElement('img');
+            selected_image.setAttribute("src", selected_article.MainImageUrl);
 
-          var selected_price = document.createElement('p');
-          var selected_price_value = document.createTextNode(selected_article.BestOffer.SalePrice + " €");
-          selected_price.appendChild(selected_price_value);
+            var selected_name = document.createElement('strong');
+            var selected_name_value = document.createTextNode(selected_article.Name);
+            selected_name.appendChild(selected_name_value);
 
+            var selected_desc = document.createElement('p');
+            var selected_desc_value = document.createTextNode(selected_article.Description);
+            selected_desc.appendChild(selected_desc_value);
 
-          selected_article_container.append(selected_image);
-          selected_article_container.append(selected_name);
-          selected_article_container.append(selected_desc);
-          selected_article_container.append(selected_price);
-          $('#selected-articles').append(selected_article_container);
-          $('#selected-articles').append(id_cdiscount);
-          $('#selected-articles').append(quantity);
+            var selected_price = document.createElement('p');
+            var selected_price_value = document.createTextNode(selected_article.BestOffer.SalePrice + " €");
+            selected_price.appendChild(selected_price_value);
 
 
-          article_count = 1;
-        }
+            selected_article_container.append(selected_image);
+            selected_article_container.append(selected_name);
+            selected_article_container.append(selected_desc);
+            selected_article_container.append(selected_price);
+            $('#selected-articles').append(selected_article_container);
+            $('#selected-articles').append(id_cdiscount);
+            $('#selected-articles').append(quantity);
 
-
-        return false;
-      });
-    },
-    error: function(a, b, errorThrown){
-      console.log(a, b, errorThrown);
-    },
-    method: "POST"
+            article_count = 1;
+          }
+          return false;
+        });
+      },
+      error: function(a, b, errorThrown){
+        console.log(a, b, errorThrown);
+      },
+      method: "POST"
+    });
+    return false;
   });
-  return false;
-});
+
+  $('#next-step').click(function(){
+    $('#step-one').css("display", "none");
+    $('#step-two').css("display", "block");
+  })
+
+  $('#previous-step').click(function(){
+    $('#step-one').css("display", "block");
+    $('#step-two').css("display", "none");
+  })
+
+  $('#add_tag').click(function(){
+    console.log($('.tags-input').val());
+    return false;
+  })
 })();
--- resources/views/create-list.blade.php
+++ resources/views/create-list.blade.php
@@ -6,17 +6,17 @@
 
 <div class="row creation">
 
-  <form id="list-creation">
+  <form id="list-creation" action="/sdgh" method="post">
     <div id="step-one">
       <h1 style="text-align: center">Etape 1 : Sélectionnez vos produits</h1>
-      <div id="articles-container"class="col-md-6 col-md-offset-3" style="padding: 10px; background-color: #FFFFFF">
+      <div id="articles-container" class="col-md-6 col-md-offset-3" style="padding: 10px; background-color: #FFFFFF">
         <h2 class="no-article">Pas encore de produits</h2>
         <div id="selected-articles">
 
         </div>
         <div class="form-steps">
           <div class="col-md-offset-5">
-            <button type="button">Etape suivante</button>
+            <button id="next-step" type="button">Etape suivante</button>
           </div>
         </div>
       </div></br>
@@ -26,7 +26,6 @@
           <div id="search-region" class="row form-group">
             <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
               <h1>Recherche de produits</h1>
-
               <input id="search-bar" type="text" class="form-control" placeholder="Entrez un nom de produit..."/>
               <button id="go-search" tabindex="1" title="Rechercher des produits">Rechercher</button>
             </div>
@@ -38,13 +37,30 @@
       </div>
     </div>
 
-    <div id="step-two">
-      <div class="info-container">
+    <div id="step-two" style="display: none;" >
+      <h1 style="text-align: center;">Etape 2 : Validez votre liste</h1>
+      <div id="info-container" class="col-md-6 col-md-offset-3" style="padding: 10px; background-color: #FFFFFF">
+        <label for="list_name">Nom de la liste :</label></br>
+        <input id="list_name" type="text" name="list_name" placeholder="Nom de la liste..."></br>
+        <label for="">Description :</label></br>
+        <textarea name="list_description" rows="6" cols="60"></textarea></br>
 
+        <div id="tag-search" class="row form-group">
+          <div class="col-md-6 ">
+            <form>
+              <input type="text" class="tags-input form-control" placeholder="Entrez un tag..."/>
+              <input id="add_tag" type="submit" tabindex="1" title="Ajouter un tag" value="Ajouter" />
+            </form>
+          </div>
+        </div>
+        <button id="previous-step" type="button">Etape précédente</button>
       </div>
-      <div class="recap-container">
 
+      <div id="recap-container"class="col-md-6 col-md-offset-3" style="padding: 10px; background-color: #FFFFFF">
+        rejdhbsjbn
       </div>
+
+      <input type="submit" name="create_list" value="Valider">
     </div>
   </form>
 
