4e2c8be83a3431b69b3f0e939e930060dddc8774
Théo Frison <theo.frison@etu.u-bordeaux.fr>
2017-11-17 20:20:44 +0100

    Revert "Revert "Merge branch 'matthieu' into front-end""
    This reverts commit ec00c5a9d249fd7937e2ccd083798c9a70bcf7db

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,79 +1,68 @@
 <div class="col-md-10 col-lg-8  col-md-offset-1 col-lg-offset-2">
-    <div class="row">
+  <div class="row">
 
-        <nav class="navbar navbar-default" role="navigation">
-            <div>
+    <nav class="navbar navbar-default" role="navigation">
+      <div>
 
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                            data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="/">
-                        <img class="logo img-responsive" src="../../../public/images/Logo_V4_transparent.png"
-                             alt="logo listin"/>
-                    </a>
-                </div>
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+          data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">
+          <img class="logo img-responsive" src="../../../public/images/Logo_V4_transparent.png"
+          alt="logo listin"/>
+        </a>
+      </div>
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a tabindex="1" href="/catalogue">
-                                <img alt="catalogue" src="../../../public/images/icon-catalogue.png"/>
-                                <h5>Catalogue</h5>
-                            </a>
-                        </li>
-                        <li>
-                            <a tabindex="1" href="/create/list">
-                                <img alt="créer une liste" src="../../../public/images/icon-new-list.png"/>
-                                <h5>Créer une liste</h5>
-                            </a>
-                        </li>
-                        <li>
-                            <a tabindex="1" href="#">
-                                <img alt="mon panier" src="../../../public/images/icon-basket.png"/>
-                                <h5>Mon Panier</h5>
-                            </a>
-                        </li>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a tabindex="1" href="/catalogue">
+              <img alt="catalogue" src="../../../public/images/icon-catalogue.png"/>
+              <h5>Catalogue</h5>
+            </a>
+          </li>
+          <li>
+            <a tabindex="1" href="/create/list">
+              <img alt="créer une liste" src="../../../public/images/icon-new-list.png"/>
+              <h5>Créer une liste</h5>
+            </a>
+          </li>
+          <li>
+            <a tabindex="1" href="#">
+              <img alt="mon panier" src="../../../public/images/icon-basket.png"/>
+              <h5>Mon Panier</h5>
+            </a>
+          </li>
 
 
+          @guest
+          <li>
+            <a href="{{ route('login') }}" tabindex="1">
+              <img alt="connexion" src="../../../public/images/icon-account.png"/>
+              <h5>Se connecter</h5>
+            </a>
 
-                            @guest
-                                <li>
-                                    <a href="{{ route('login') }}" tabindex="1">
-                                        <img alt="connexion" src="../../../public/images/icon-account.png"/>
-                                        <h5>Se connecter</h5>
-                                    </a>
+          </li>
+          @else
+          <li>
+            <a tabindex="1" href="#">
+              <img alt="mon compte" src="../../../public/images/icon-account.png"/>
+              <h5>Mon Compte</h5>
+            </a>
+          </li>
+          @endguest
 
-                                </li>
-                                @else
-                                    <li>
-                                        <a tabindex="1" href="#">
-                                            <img alt="mon compte" src="../../../public/images/icon-account.png"/>
-                                            <h5>Mon Compte</h5>
-                                        </a>
-                                    </li>
-                                    <!--
-                                    <li>
-                                     <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                         <img alt="mon compte" src="../../../public/images/icon-deconnexion.png"/>
-                                         <h5>Se déconnecter</h5>
-                                     </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                    </li>
-                                    -->
-                            @endguest
-                    </ul>
-                </div>
-
-            </div>
-        </nav>
+        </ul>
+      </div>
 
     </div>
+  </nav>
+
+</div>
+</div>
 </div>
