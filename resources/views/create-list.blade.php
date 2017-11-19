d3f2c65e2016958f38f2de0e37048a26f2fe1005
Cedric Frecheville <frecheville.cedric@gmail.com>
2017-11-19 21:41:56 +0100

    css on create-liste

--- resources/views/create-list.blade.php
+++ resources/views/create-list.blade.php
@@ -18,20 +18,20 @@
                     </div>
                     <div class="form-steps">
                         <div class="col-md-offset-5">
-                            <button id="next-step" type="button">Etape suivante</button>
+                            <button class="btn btn-next-step" id="next-step" type="button">Etape suivante</button>
                         </div>
                     </div>
                 </div>
                 <br>
 
-                <div class="col-md-6 col-md-offset-3" style="padding: 10px; margin-top: 3%; background-color: #FFFFFF">
+                <div class="research-product col-md-6 col-md-offset-3">
                     <div class="col-xs-12">
                         <div id="search-region" class="row form-group">
                             <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
                                 <h1>Recherche de produits</h1>
-                                <input id="search-bar" type="text" class="form-control"
+                                <input class="search-bar" id="search-bar" type="text" class="form-control"
                                        placeholder="Entrez un nom de produit..."/>
-                                <button id="go-search" tabindex="1" title="Rechercher des produits">Rechercher</button>
+                                <button class="btn btn-go-search" id="go-search" tabindex="1" title="Rechercher des produits">Rechercher</button>
                             </div>
                         </div>
                         <div id="result-region">
@@ -42,24 +42,23 @@
             </div>
 
             <div id="step-two" style="display: none;">
-                <h1 style="text-align: center;">Etape 2 : Validez votre liste</h1>
-                <div id="info-container" class="col-md-6 col-md-offset-3"
-                     style="padding: 10px; background-color: #FFFFFF">
-                    <label for="list_name">Nom de la liste :</label><br>
-                    <input id="list_name" type="text" name="list_name" placeholder="Nom de la liste..."><br>
-                    <label for="">Description :</label><br>
-                    <label for="list_description"></label><textarea id="list_description" name="list_description" rows="6" cols="60"></textarea><br>
+                <h1 class="title-create-list">Etape 2 : Validez votre liste</h1>
+                <div id="info-container" class="col-md-6 col-md-offset-3">
+                    <label for="list_name" class="name-list">Nom de la liste :</label><br>
+                    <input id="list_name" class="name_liste" type="text" name="list_name" placeholder="Nom de la liste..."><br>
+                    <label for="" class="description-list">Description :</label><br>
+                    <label for="list_description"></label><textarea class="description_list" id="list_description" name="list_description" rows="6" cols="60"></textarea><br>
 
                     <div id="tag-search" class="row form-group">
                         <div class="col-md-6 ">
                             <form>
                                 <input id="selected_tag" type="text" class="tags-input form-control"
                                        placeholder="Entrez un tag..."/>
-                                <input id="add_tag" type="submit" tabindex="1" title="Ajouter un tag" value="Ajouter"/>
+                                <input class="btn btn-add-tags" id="add_tag" type="submit" tabindex="1" title="Ajouter un tag" value="Ajouter"/>
                             </form>
                         </div>
                     </div>
-                    <button id="previous-step" type="button">Etape précédente</button>
+                    <button class="previous-step" id="previous-step" type="button">Etape précédente</button>
                 </div>
 
                 <div id="recap-container" class="col-md-6 col-md-offset-3"
